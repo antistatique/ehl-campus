6f4ea312321da0c59634ff1742516ce078832735
gagarine <perdrisat@gmail.com>
2012-12-13 01:00:47 +0100

    top bar

--- drupal/sites/all/themes/ehl/template.php
+++ drupal/sites/all/themes/ehl/template.php
@@ -19,3 +19,14 @@ function ehl_breadcrumb($variables) {
   return theme_breadcrumb($variables);
 }
 
+function ehl_menu_tree($variables) {
+  return '<ul class="nav">' . $variables['tree'] . '</ul>';
+}
+
+function ehl_preprocess_block(&$variables) {
+  if ($variables['block_html_id'] === 'block-search-form') {
+    $variables['classes_array'][] = 'pull-right';
+  } else if ($variables['block_html_id'] === 'block-system-user-menu') {
+    $variables['classes_array'][] = 'pull-right';
+  }
+}
