3fd9d8051ea3924ec54d7881d0f153cba5a42474
gagarine <perdrisat@gmail.com>
2012-12-18 01:41:04 +0100

    create a public settings file

---
+++ drupal/sites/default/public.settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * IMPORTANT: File visible PUBLICLY (on the git repository)
+ * Do not put any sensitive information like credential
+ * If you are not sure... ask.
+ *
+ * @file
+ * Configuration file shared on every host
+ *
+ */
+
+$conf['locale_custom_strings_en'][''] = array(
+   'Username'      => 'Full Name',
+ );
