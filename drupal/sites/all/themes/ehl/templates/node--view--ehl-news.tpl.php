68979ff3e160354897799d1c763f981247c2bb45
Yann Gouffon <yago@yago-art.ch>
2013-01-09 17:02:26 +0100

    css adjustment for news layout

--- drupal/sites/all/themes/ehl/templates/node--view--ehl-news.tpl.php
+++ drupal/sites/all/themes/ehl/templates/node--view--ehl-news.tpl.php
@@ -17,30 +17,35 @@ hide($content['field_logo']);
 hide($content['field_badge'])
 ?>
 
-<!-- node.tpl.php -->
+<!-- node--view--ehl-news.tpl.php -->
 <article <?php print $id_node . $classes .  $attributes; ?> role="article">
+  <div class="span12">
+    <div class="row">
 
-    <div class="span2">
-      <?php print $user_picture; ?>
-      <span class="author"><?php print t('Written by'); ?> <?php print $name; ?></span><br />
-      <span class="date"><?php print t('On the'); ?> <time><?php print $date; ?></time></span><br />
+      <div class="span2 sidebar">
+        <?php print $user_picture; ?>
+        <span class="author"><?php print t('Written by'); ?> <?php print $name; ?></span><br />
+        <span class="date"><?php print t('On the'); ?> <time><?php print $date; ?></time></span><br />
 
-      <?php if(module_exists('comment')): ?>
-        <span class="comments"><?php print $comment_count; ?> Comments</span>
-      <?php endif; ?>
-    </div>
-
-    <div class="span9 offset1 news-content">
-      <?php print render($title_prefix); ?>
-      <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <div class="content">
-        <?php print render($content);?>
+        <?php if(module_exists('comment')): ?>
+          <span class="comments"><?php print $comment_count; ?> Comments</span>
+        <?php endif; ?>
       </div>
-      <?php print render($content['links']); ?>
-      <?php print render($content['comments']); ?>
+
+      <div class="span9 offset1 news-content">
+        <?php print render($title_prefix); ?>
+        <?php if (!$page): ?>
+          <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <div class="content">
+          <?php print render($content);?>
+        </div>
+        <?php print render($content['links']); ?>
+        <?php print render($content['comments']); ?>
+      </div>
+
     </div>
+  </div>
 
 </article>
