e70e1c268f5e42bff94b231dcf40e260378b6c34
Yann Gouffon <yago@yago-art.ch>
2013-01-09 12:37:25 +0100

    add no-margin class every 4 item to fix bootstrap failure

--- drupal/sites/all/themes/ehl/js/script.js
+++ drupal/sites/all/themes/ehl/js/script.js
@@ -23,4 +23,8 @@
     $('.map').animate({height:580},700);
   });
 
+  // Project //////////////////////////////////////////
+
+  $(".page-projects .view-project-list .view-filters + .span3, .page-projects .view-project-list .span3:nth-child(4n+2)").addClass("no-margin");
+
 })(jQuery);
