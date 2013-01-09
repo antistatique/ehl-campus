d2c21559de6a04aed400a70f07ed57ceca589590
Yann Gouffon <yago@yago-art.ch>
2013-01-09 16:42:31 +0100

    add news template

---
+++ drupal/sites/all/themes/ehl/templates/node--view--ehl-news.tpl.php
@@ -0,0 +1,46 @@
+<?php
+//kpr(get_defined_vars());
+//http://drupalcontrib.org/api/drupal/drupal--modules--node--node.tpl.php
+//node--[CONTENT TYPE].tpl.php
+
+if ($classes) {
+  $classes = ' class="'. $classes . ' "';
+}
+
+if ($id_node) {
+  $id_node = ' id="'. $id_node . '"';
+}
+
+hide($content['comments']);
+hide($content['links']);
+hide($content['field_logo']);
+hide($content['field_badge'])
+?>
+
+<!-- node.tpl.php -->
+<article <?php print $id_node . $classes .  $attributes; ?> role="article">
+
+    <div class="span2">
+      <?php print $user_picture; ?>
+        <span class="author"><?php print t('Written by'); ?> <?php print $name; ?></span>
+        <span class="date"><?php print t('On the'); ?> <time><?php print $date; ?></time></span>
+
+        <?php if(module_exists('comment')): ?>
+          <span class="comments"><?php print $comment_count; ?> Comments</span>
+        <?php endif; ?>
+    </div>
+
+    <div class="span9 offset1">
+      <?php print render($title_prefix); ?>
+      <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <div class="content">
+        <?php print render($content);?>
+      </div>
+      <?php print render($content['links']); ?>
+      <?php print render($content['comments']); ?>
+    </div>
+
+</article>
