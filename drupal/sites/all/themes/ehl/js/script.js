a8262011e16b314b40c8f491e1a3509c24f7e022
Yann Gouffon <yago@yago-art.ch>
2013-01-10 16:14:53 +0100

    clear script

--- drupal/sites/all/themes/ehl/js/script.js
+++ drupal/sites/all/themes/ehl/js/script.js
@@ -1,14 +1,20 @@
-
 (function ($) {
 
-  var $maxHeight = 580;
-  var $minHeight = 155;
+  var $mapHeight = [ 580, 155 ];
+  var newHeight = 0;
 
   var $mapBlock = $('.map');
   var $toggleButton = $("#toggle-button");
 
-  // Set craftmap ////////////////////////////////////////////////////
-  
+  $(document).ready(function(){
+    craftMap();
+    defineHeight();
+    toggleButton();
+  });
+
+  /**
+   * init craftmap.js
+   */
   function craftMap(){
     $('.map').craftmap({
       image: {
@@ -21,61 +27,75 @@
     });
   }
 
-  craftMap();
 
-  // Init map ///////////////////////////////////////////////////////
+  /**
+   * Define de height of the map's wrapper
+   */
+  function defineHeight(){
+    if($.cookie("ehl-forum") == null){
+      $.cookie("ehl-forum", "1");
+      $mapBlock.height($mapHeight[0]);
+      openTooltip();
+    }else if($.cookie("ehl-forum") == "1"){
+      openTooltip();
+      $mapBlock.height($mapHeight[0]);
+    }else {
+      $toggleButton.addClass('top');
+      $mapBlock.height($mapHeight[1]);
+    }
+    console.log('height : ' + $mapBlock.height());
+  }
 
+
+  /**
+   * Open the tooltip who's set
+   */
   function openTooltip(){
     setTimeout(function(){ 
       $('#southkorea-marker').trigger('click');
     }, 700);
   };
 
-  if($.cookie("ehl-forum") == null){
-    var $mapHeight = $maxHeight;
-  }else if($.cookie("ehl-forum") == $maxHeight){
-    $(document).ready(function() {
-      openTooltip();
-    });
-  }else {
-    $toggleButton.addClass('top');
-  }
 
-  $.cookie("ehl-forum", $mapHeight);
-
-  $mapBlock.height($.cookie("ehl-forum"));
-
-  // Set slide up/down on map ////////////////////////////////////////
+  /**
+   * Set slide up/down on map
+   */
   function animationHeight(){
-    $mapBlock.animate({height:$.cookie("ehl-forum")},700);
+    $mapBlock.animate({height:$newHeight},700);
   }
 
-  if($.cookie("ehl-forum") == $maxHeight){
-    $toggleButton.toggle(function(){
-      $mapHeight = $minHeight;
-      $.cookie("ehl-forum", $mapHeight);
-      animationHeight();
-      $(this).addClass('top');
-    },function(){
-      $mapHeight = $maxHeight;
-      $.cookie("ehl-forum", $mapHeight);
-      animationHeight();
-      openTooltip();
-      $(this).removeClass('top');
-    });
-  }else {
-    $toggleButton.toggle(function(){
-      $mapHeight = $maxHeight;
-      $.cookie("ehl-forum", $mapHeight);
-      animationHeight();
-      openTooltip();
-      $(this).removeClass('top');
-    },function(){
-      $mapHeight = $minHeight;
-      $.cookie("ehl-forum", $mapHeight);
-      animationHeight();
-      $(this).addClass('top');
-    });
+
+  /**
+   * Toggle button for opening/closing the map
+   */
+  function toggleButton(){
+    if($.cookie("ehl-forum") == "1"){
+      $toggleButton.toggle(function(){
+        $newHeight = $mapHeight[1];
+        animationHeight();
+        $(this).addClass('top');
+        $.cookie("ehl-forum", "0");
+      },function(){
+        $newHeight = $mapHeight[0];
+        animationHeight();
+        openTooltip();
+        $(this).removeClass('top');
+        $.cookie("ehl-forum", "1");
+      });
+    }else {
+      $toggleButton.toggle(function(){
+        $newHeight = $mapHeight[0];
+        animationHeight();
+        openTooltip();
+        $(this).removeClass('top');
+        $.cookie("ehl-forum", "1");
+      },function(){
+        $newHeight = $mapHeight[1];
+        animationHeight();
+        $(this).addClass('top');
+        $.cookie("ehl-forum", "0");
+      });
+    }
   }
 
 })(jQuery);
