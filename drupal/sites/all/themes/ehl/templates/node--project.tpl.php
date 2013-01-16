7bd1f436d04e8a6ed39ea8eca3c647ef4ba3d5ce
gagarine <simon@antistatique.net>
2013-01-16 11:52:20 +0100

    cover image in project

--- drupal/sites/all/themes/ehl/templates/node--project.tpl.php
+++ drupal/sites/all/themes/ehl/templates/node--project.tpl.php
@@ -13,8 +13,8 @@ if ($id_node) {
 
 hide($content['comments']);
 hide($content['links']);
-hide($content['field_logo']);
-hide($content['field_badge'])
+//hide($content['field_logo']);
+//hide($content['field_badge'])
 ?>
 
 <!-- node--view--ehl-news.tpl.php -->
@@ -23,13 +23,15 @@ hide($content['field_badge'])
     <div class="row">
 
       <div class="span9 news-content">
+        <?php print render($content['field_project_cover_image']); ?>
+        
         <?php print render($title_prefix); ?>
         <?php if (!$page): ?>
           <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
         <div class="content">
-          <?php print render($content['body']);?>
+          <?php print render($content);?>
         </div>
         <?php print render($content['links']); ?>
       </div>
