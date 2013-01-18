50d0cf0276ad6b8aa2e0cf14338fd3ed4b32ad80
gagarine <simon@antistatique.net>
2013-01-18 19:25:07 +0100

    footer region tpl

--- drupal/sites/all/themes/ehl/templates/page--front.tpl.php
+++ drupal/sites/all/themes/ehl/templates/page--front.tpl.php
@@ -193,23 +193,7 @@
   </div><!--/page-->
 </div>
 
-<div class="footer">
-    <div class="container">
-        <div class="row-fluid">
-            <div class="span6">
-              <h1>Campus Development Forum</h1>
-              <h2>International students workshop</h2>
-              <p>
-                Campus dev forum is the place for you to collect, share and discuss your ideas and concepts with other participants from all over the world. You can even get feedback from them. We invite you to post your thoughts and inspiration about the workshop and complete your project page with your concept.
-              </p>
-              <p>
-                Our goal is to build a campus made by students for students of <a href="http://ehl.edu/eng/About-us">Ecole hôtelière de Lausanne</a>. If you have any questions. Email us at <a href="mailto:contact@campusdevforum.net">contact@campusdevforum.net</a>. We are here to help.
-              </p>
-            </div>
-            <div class="span5 offset1">
-                <?php print render($page['footer']); ?>
-            </div>
-        </div>
-    </div>
+<div class="footer">  
+  <?php print render($page['footer']); ?>
 </div>
 
