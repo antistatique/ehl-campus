c33e3b4060284a9c232ef831756d88e08f00a3db
gagarine <simon@antistatique.net>
2013-01-29 16:06:31 +0100

    Change footer text #75

--- drupal/sites/all/themes/ehl/templates/region--footer.tpl.php
+++ drupal/sites/all/themes/ehl/templates/region--footer.tpl.php
@@ -5,10 +5,13 @@
           <h1>Campus Development Forum</h1>
           <h2>International students workshop</h2>
           <p>
-            Campus dev forum is the place for you to collect, share and discuss your ideas and concepts with other participants from all over the world. You can even get feedback from them. We invite you to post your thoughts and inspiration about the workshop and complete your project page with your concept.
+            The Campus Development Forum project aims to build a campus designed by students for students of the <a href="http://ehl.edu/eng/About-us">Ecole hôtelière de Lausanne</a>. 
           </p>
           <p>
-            Our goal is to build a campus made by students for students of <a href="http://ehl.edu/eng/About-us">Ecole hôtelière de Lausanne</a>. If you have any questions. Email us at <a href="mailto:contact@campusdevforum.net">contact@campusdevforum.net</a>. We are here to help.
+            On this web platform, you can collect, share and discuss your ideas and concepts with other participants from all over the world. You can even get feedback from them. We invite you to post your thoughts and inspiration about the workshop, and complete your project page with your concept. 
+          </p>
+          <p>
+            If you have any questions, email us at <a href="mailto:contact@campusdevforum.net">contact@campusdevforum.net</a>. We are here to help! 
           </p>
         </div>
         <div class="span5 offset1">
@@ -16,3 +19,7 @@
        	</div>
     </div>
 </div>
+
+
+
+
